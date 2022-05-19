e4528d7c19aedf1aedf311e50d4319039c738ef6
Olive <olivelliott48@gmail.com>
2022-05-18 21:59:06 -0400

    create time function " q :q "" clear
    :Q
    git commit -m"create time function"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
+var currentTimeEl = $('#currentDay')
 var scheduleContainerEl = $('.schedule-container');
-var timeOfDayEl = $('.hour')
+var scheduleTimeEl = $('.hour')
 var taskInputContainerEl = $('.description');
 var saveButtonContainerEl = $('.saveBtn');
 var scheduleTimesEl = ['9:00am', '10:00am','11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm',  '4:00pm', '5:00pm'];
@@ -7,9 +8,14 @@ var scheduleTasksEl = [];
 var toDoEl = {};
 
 
+function createTime () {
+    var timeDisplay = moment().format('ddd, MMM do YYYY, h:mm a');
+    currentTimeEl.append(timeDisplay);
+    console.log(timeDisplay);
+    console.log(currentTimeEl)
+}
 
-
-
+createTime();
 
 
 
