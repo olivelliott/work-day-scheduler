ec3420cb3b2fdc221e10d440da6395565e15465c
Olive <olivelliott48@gmail.com>
2022-05-21 18:34:28 -0400

    Update time status function variables for readability and add classes for color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,43 @@
 var saveBtnEl = $('.saveBtn');
 
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+$('#currentDay').text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
 function checkTimeStatus() {
-    var hour = moment().hours();
-    $(".time-block").each(function() {
-        var currHour = parseInt($(this).attr("id"));
-        if (currHour > hour) {
-            $(this).addClass("future");
-        } else if (currHour === hour) {
-            $(this).addClass("present");
+    var hour = moment().hour();
+    $('.time-block').each(function() {
+        var timeBlockHour = parseInt($(this).attr('id'));
+        if (timeBlockHour > hour) {
+            $(this).addClass('future');
+        } else if (timeBlockHour === hour) {
+            $(this).removeClass('future');
+            $(this).removeClass('past');
+            $(this).addClass('present');
         } else {
-            $(this).addClass("past");
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+            $(this).addClass('past');
         }
     })
 };
 
 var loadTasks = function() {
-    $(".hour").each(function() {
+    $('.hour').each(function() {
         var currHour = $(this).text();
         var currPlan = localStorage.getItem(currHour);
         if(currPlan !== null) {
-            $(this).siblings(".plan").val(currPlan);
+            $(this).siblings('.plan').val(currPlan);
         }
     });
 }
 
 $(saveBtnEl).on('click', function () {
-    var time = $(this).siblings(".hour").text();
-    var plan = $(this).siblings(".plan").val();
+    var time = $(this).siblings('.hour').text();
+    var plan = $(this).siblings('.plan').val();
     localStorage.setItem(time, plan);
 });
 
 checkTimeStatus();
-// setInterval(checkTimeStatus(), (1000 * 60) * 5);
+setInterval(checkTimeStatus(), (1000 * 60) * 5);
 loadTasks();
 
 
